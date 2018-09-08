5ebda1f5dcb572c317f35376f868831c367ad2c4
Adam Bergstein <nerdstein@gmail.com>
2018-09-07 20:51:32 -0400

    Clean up

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -768,4 +768,4 @@ $settings['entity_update_batch_size'] = 50;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$config_directories['sync'] = $app_root . '../../config/sync';
+$config_directories['sync'] = $app_root . '/../config/sync';
