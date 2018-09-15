02176e1a6c71c673dd7608d601096e6a177a47db
Adam Bergstein <nerdstein@gmail.com>
2018-09-15 16:07:07 -0400

    blog list twig

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -769,3 +769,8 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 $config_directories['sync'] = $app_root . '/../config/sync';
+
+// Automatically generated include for settings managed by ddev.
+/*if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}*/
